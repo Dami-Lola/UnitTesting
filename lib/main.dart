ffdc7616513fcf2af2d745b2799f5cef20f6f416
Dami-lola <damiokafor@gmail.com>
2021-04-27 00:02:05 +0100

    Login Logic Unit Tess

--- lib/main.dart
+++ lib/main.dart
@@ -20,6 +20,23 @@ class MyApp extends StatelessWidget {
   }
 }
 
+class UsernameValidator {
+  static String validate(String value) {
+    return value.isEmpty ? 'Username can\'t be empty' : null;
+  }
+}
+
+class EmailFieldValidator {
+  static String validate(String value) {
+    return value.isEmpty ? 'Email can\'t be empty' : null;
+  }
+}
+
+class PasswordFieldValidator {
+  static String validate(String value) {
+    return value.isEmpty ? 'Password can\'t be empty' : null;
+  }
+}
 
 class LoginPage extends StatefulWidget {
   LoginPage({Key key}) : super(key: key);
@@ -45,113 +62,92 @@ class _LoginPageState extends State<LoginPage> {
       body: Padding(
         padding: EdgeInsets.all(16),
         child: Form(
-          autovalidateMode: AutovalidateMode.disabled, key: _formKey,
+          autovalidateMode: AutovalidateMode.disabled,
+          key: _formKey,
           child: new ListView(
-              children: <Widget>[
-                Container(
-                  padding: EdgeInsets.symmetric(vertical: 8),
-                  width: 250,
-                  height: 70,
-                  child: TextFormField(
-                    key: Key('username'),
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'UserName',
-                      hintText: 'Enter username eg lola.vola',
+            children: <Widget>[
+              Container(
+                padding: EdgeInsets.symmetric(vertical: 8),
+                width: 250,
+                height: 70,
+                child: TextFormField(
+                  key: Key('username'),
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'UserName',
+                    hintText: 'Enter username eg lola.vola',
+                  ),
+                  validator: UsernameValidator.validate,
+                  onSaved: (String value) => _username = value,
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.symmetric(vertical: 8),
+                width: 250,
+                height: 70,
+                child: TextFormField(
+                  key: Key('email'),
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Email address',
+                    hintText: 'Enter your email address',
+                  ),
+                  validator: EmailFieldValidator.validate,
+                  onSaved: (String value) => _email = value,
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.symmetric(vertical: 8),
+                width: 250,
+                height: 70,
+                child: TextFormField(
+                  key: Key('password'),
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Password',
+                    hintText: 'Enter your password',
+                  ),
+                  validator: PasswordFieldValidator.validate,
+                  onSaved: (String value) => _password = value,
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.symmetric(vertical: 25),
+                width: 200.00,
+                height: 50.00,
+                child: ElevatedButton(
+                  child: Text(
+                    'Register',
+                    style: TextStyle(fontSize: 20.0, color: Colors.white),
+                  ),
+                  style:
+                      ElevatedButton.styleFrom(primary: Colors.lightBlue[200]),
+                  onPressed: () {
+                    if (_formKey.currentState.validate()) {
+                      Navigator.of(context).push(
+                          MaterialPageRoute(builder: (context) => HomePage()));
+                    } else {
+                      print('Don\'t be silly');
+                    }
+                  },
+                ),
+              ),
+              RichText(
+                text: TextSpan(
+                  text: "Have an account?",
+                  style: TextStyle(color: Colors.black, fontSize: 14),
+                  children: <TextSpan>[
+                    TextSpan(
+                      text: ' Sign in',
+                      style: TextStyle(color: Colors.blue, fontSize: 14),
+                      recognizer: TapGestureRecognizer()..onTap = () {},
                     ),
-                    validator: (value) => value.isEmpty ? 'Username can\'t be empty' : null,
-                    onSaved: (String value) => _username = value,
-                  ),
+                  ],
                 ),
-                Container(
-                  padding: EdgeInsets.symmetric(vertical: 8),
-                  width: 250,
-                  height: 70,
-                  child: TextFormField(
-                    key: Key('email'),
-                    obscureText: true,
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'Email address',
-                      hintText: 'Enter your email address',
-                    ),
-                    validator: (value) => value.isEmpty ? 'Username can\'t be empty' : null,
-                    onSaved: (String value) => _email = value,
-                  ),
-                ),
-                Container(
-                  padding: EdgeInsets.symmetric(vertical: 8),
-                  width: 250,
-                  height: 70,
-                  child: TextFormField(
-                    key: Key('password'),
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'Password',
-                      hintText: 'Enter your password',
-                    ),
-                    validator: (value) => value.isEmpty ? 'Username can\'t be empty' : null,
-                    onSaved: (String value) => _password = value,
-                  ),
-                ),
-                Container(
-                  margin: EdgeInsets.symmetric(vertical: 25),
-                  width: 200.00,
-                  height: 50.00,
-                  child: ElevatedButton(
-                      child: Text(
-                        'Register',
-                        style: TextStyle(fontSize: 20.0, color: Colors.white),
-                      ),
-                      style: ElevatedButton.styleFrom(primary: Colors.lightBlue[200]),
-                      onPressed: () {
-                        if (_formKey.currentState.validate()) {
-                          Navigator.of(context).push(MaterialPageRoute(builder: (context) => HomePage()));
-                        }
-                        else {
-                          print('Don\'t be silly');
-                        }
-                      }
-                  ),
-                ),
-                RichText(
-                  text: TextSpan(
-                    text: "Have an account?",
-                    style: TextStyle(color: Colors.black, fontSize: 14),
-                    children: <TextSpan>[
-                      TextSpan(
-                        text: ' Sign in',
-                        style: TextStyle(color: Colors.blue, fontSize: 14),
-                        recognizer: TapGestureRecognizer()
-                          ..onTap = () {},
-                      ),
-                    ],
-                  ),
-                ),
-                ],
+              ),
+            ],
           ),
-         /*   Text(
-              "Good Day!",
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-                color: Colors.black,
-                wordSpacing: 5,
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 8),
-              child: Text(
-                "Please fill in this form to create a new account",
-                style: TextStyle(
-                  fontSize: 14,
-                  fontWeight: FontWeight.normal,
-                  color: Colors.grey,
-                ),
-              ),
-            ),
-
-          */
         ),
       ),
     );
--- test/login_page_test.dart
+++ test/login_page_test.dart
@@ -4,16 +4,39 @@ import 'package:flutter_app/main.dart';
 
 
 void main (){
+  //Unit tests for validating login page
+  test('empty username returns error string', () {
 
-  testWidgets("Flutter textField Test",  (WidgetTester tester) async {
+    final result = UsernameValidator.validate('');
+    expect(result, 'Username can\'t be empty');
+  });
 
-    LoginPage loginPage = LoginPage();
+  test('non-empty username returns null', () {
+    final result = UsernameValidator.validate('username');
+    expect(result, null);
+  });
 
-    await tester.pumpWidget(MyApp());
-    var userName = find.byType(TextField);
-    expect(userName, findsWidgets);
-    await tester.enterText(userName, 'LolaVola');
-    expect(find.text('LolaVola'), findsOneWidget);
-    print('LolaVola');
+  test('empty email returns error string', () {
+
+    final result = EmailFieldValidator.validate('');
+    expect(result, 'Email can\'t be empty');
+  });
+
+  test('non-empty email returns null', () {
+
+    final result = EmailFieldValidator.validate('email');
+    expect(result, null);
+  });
+
+  test('empty password returns error string', () {
+
+    final result = PasswordFieldValidator.validate('');
+    expect(result, 'Password can\'t be empty');
+  });
+
+  test('non-empty password returns null', () {
+
+    final result = PasswordFieldValidator.validate('password');
+    expect(result, null);
   });
 }
